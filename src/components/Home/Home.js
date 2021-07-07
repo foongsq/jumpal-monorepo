a5b6943abbf4e1570e183587a45b0a67ab6882fc
foongsiqi <edu.foongs@gmail.com>
2021-07-07 16:38:56 +0800

    Tweak styling to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,23 +7,18 @@ import './Home.css';
 export default class Home extends React.Component {
   render() {
     return (
-      <div className="outerContainer">
-        <div className='home-container'>
-          <button className="speedFreestyleButtons" href="/Speed">
-          <img src={speed} className='motivation-img' alt='' />
-          <Link to='/Speed' className="linkSpeed">Speed</Link>
-          </button>
-          <button className="speedFreestyleButtons">
-          <img src={freestyle} className='motivation-img' alt='' />
-          <Link to='/Freestyle' className="linkSpeed">Freestyle</Link></button>
-          {/* <h2 style={{textAlign:'center'}}>Motivational Quotes</h2>
-          <img src={quote1} className='motivation-img' alt='' />
-          <img src={quote2} className='motivation-img' alt='' />
-          <img src={quote3} className='motivation-img' alt='' />
-          <img src={quote4} className='motivation-img' alt='' />
-          <img src={quote5} className='motivation-img' alt='' />
-          <img src={quote6} className='motivation-img' alt='' />
-          <img src={quote7} className='motivation-img' alt='' /> */}
+      <div className='componentContentDiv'>
+        <div className='homeContainerDiv'>
+           
+            <Link to='/Speed' className='speedFreestyleLink'>
+              <img src={speed} className='speedFreestyleButtonImage' alt='' />
+              <p>Speed</p>
+            </Link>
+            
+            <Link to='/Freestyle' className='speedFreestyleLink'>
+              <img src={freestyle} className='speedFreestyleButtonImage' alt='' />
+              <p>Freestyle</p>
+            </Link>
         </div>
       </div>
     );
