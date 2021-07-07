c998c8a47b75d174824ceec3014afeaded97d430
foongsiqi <edu.foongs@gmail.com>
2021-07-07 17:09:48 +0800

    Tweak logos and make it more responsive

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,6 +3,7 @@ import { withFirebase } from '../../Firebase/index';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from "../../images/jumpal-logo.png";
+import './Navigation.css';
 
 
 class Navigation extends React.Component {
@@ -15,18 +16,21 @@ class Navigation extends React.Component {
   }
   render() {
     return (
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/Home"><img className="logo" src={logo} alt='Logo'/></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/Home">Home</Nav.Link>
-            <Nav.Link href="/Speed">Speed</Nav.Link>
-            <Nav.Link href="/Freestyle">Freestyle</Nav.Link>
-            <Nav.Link href="/Profile">Profile</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <div>
+        <img className="topLogo" src={logo} alt='Logo'/>
+        <Navbar bg="light" expand="sm">
+          <Navbar.Brand href="/Home"><img className="logo" src={logo} alt='Logo'/></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/Home">Home</Nav.Link>
+              <Nav.Link href="/Speed">Speed</Nav.Link>
+              <Nav.Link href="/Freestyle">Freestyle</Nav.Link>
+              <Nav.Link href="/Profile">Profile</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
