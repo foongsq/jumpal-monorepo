c98994b5d40777b50c54669f5c40e38fc8596f75
foongsiqi <edu.foongs@gmail.com>
2021-07-07 16:40:00 +0800

    Change logo and update routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,28 +9,32 @@ import SignInPage from '../SignIn/SignIn';
 import Profile from '../Profile/Profile';
 import SignOut from '../SignOut/SignOut';
 import Speed from '../Speed/Speed';
+import TimingTracks from '../Speed/TimingTracks/TimingTracks';
+import Counter from '../Speed/Counter/Counter';
+import SpeedData from '../Speed/SpeedData/SpeedData';
+import PersonalBests from '../Speed/PersonalBests/PersonalBests';
 import Freestyle from '../Freestyle/Freestyle';
 import Home from '../Home/Home';
-import logo from "../../images/jumpal-logo.gif";
 
 class App extends React.Component {
   render() {
     return (
         <div className="app">
-          <div className="logoContainer">
-            <img className="logo" src={logo} alt='Logo'/>
-          </div>
           <Router>
             <div className="navAndcontent">
               <Navigation />
               <div className="content">
                 <Route exact path="/"><Redirect to="/Home" /></Route>
-                <Route path='/SignIn' component={SignInPage} />
-                <Route path='/Home' component={Home} />
-                <Route path='/Speed' component={Speed}/>
-                <Route path='/Freestyle' component={Freestyle} />
-                <Route path='/Profile' component={Profile} />
-                <Route path='/SignOut' component={SignOut} />
+                <Route path='/signin' component={SignInPage} />
+                <Route path='/home' component={Home} />
+                <Route path='/speed' component={Speed}/>
+                <Route path='/freestyle' component={Freestyle} />
+                <Route path='/profile' component={Profile} />
+                <Route path='/signout' component={SignOut} />
+                <Route path='/speed/TimingTracks' component={TimingTracks} />
+                <Route path='/speed/Counter' component={Counter} />
+                <Route path='/speed/speeddata' component={SpeedData} />
+                <Route path='/speed/personalbests' component={PersonalBests} />
               </div>
             </div>
           </Router>
