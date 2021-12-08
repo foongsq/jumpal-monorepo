6628530c61e98c9f433d0cf429ac99873a2ea3ec
foongsiqi <edu.foongs@gmail.com>
2021-12-08 21:36:55 +0800

    Fix issue with speed table overflow

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ import './Home.css';
 export default class Home extends React.Component {
   render() {
     return (
-      <div className='componentContentDiv'>
+      <div className='componentContentDiv' style={{height: '90vh'}}>
           <div className='speedFreestyleDiv'>
             <Link to='/Speed' className='speedFreestyleButton'><p>Speed</p></Link>
             <Link to='/Freestyle' className='speedFreestyleButton'><p>Freestyle</p></Link>
