ac59bde11e6f3d83ca552fc005b724654afa3c94
siqi <edu.foongs@gmail.com>
2022-06-12 14:28:28 +0800

    fix: Remove unnecessary prettier config file

--- apps/frontend/.eslintrc.js
+++ apps/frontend/.eslintrc.js
@@ -1,41 +1,41 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+  extends: [
+    "plugin:react/recommended",
+    "google",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  'plugins': [
-    'react',
-    '@typescript-eslint',
-  ],
-  'rules': {
-    'linebreak-style': ['error', 'unix'],
-    'object-curly-spacing': ['error', 'always'],
-    'require-jsdoc': ['error', {
-      'require': {
-        'FunctionDeclaration': false,
-        'MethodDefinition': false,
-        'ClassDeclaration': false,
-        'ArrowFunctionExpression': false,
-        'FunctionExpression': false,
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "linebreak-style": ["error", "unix"],
+    "object-curly-spacing": ["error", "always"],
+    "require-jsdoc": [
+      "error",
+      {
+        require: {
+          FunctionDeclaration: false,
+          MethodDefinition: false,
+          ClassDeclaration: false,
+          ArrowFunctionExpression: false,
+          FunctionExpression: false,
+        },
       },
-    }],
-    'react/jsx-key': 0,
+    ],
+    "react/jsx-key": 0,
   },
 };
