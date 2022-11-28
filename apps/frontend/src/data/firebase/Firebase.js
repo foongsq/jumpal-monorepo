d87f00ebe2e93b2344ee20342ca4c7f1efc5cec4
siqi <edu.foongs@gmail.com>
2022-11-28 17:14:37 +0800

    Fix config file issue

--- apps/frontend/src/data/firebase/Firebase.js
+++ apps/frontend/src/data/firebase/Firebase.js
@@ -2,10 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
 import { getDatabase, ref, child } from "firebase/database";
-import { config } from "./config";
 import React from "react";
 
 export const FirebaseContext = React.createContext(null);
+
+// API keys for Firebase services are ok to include in code or checked-in config files.
+const config = {
+  apiKey: "AIzaSyB_czfEJ_qq-H58aEHgKV1YyZXc_YzmbAs",
+  authDomain: "jumpal-27f91.firebaseapp.com",
+  databaseURL: "https://jumpal-27f91.firebaseio.com",
+  projectId: "jumpal-27f91",
+  storageBucket: "jumpal-27f91.appspot.com",
+  messagingSenderId: "867715616373",
+  appId: "1:867715616373:web:30087c19543aeacae7b23c",
+  measurementId: "G-T3DR03KQQ3",
+};
 
 const firebase = initializeApp(config);
 
